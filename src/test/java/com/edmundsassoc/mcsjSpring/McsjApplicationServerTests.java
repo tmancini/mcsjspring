39a5c44a57037c63dee47303b5bbddad80814cbf
TomM <TomM@TOMM.edmundsassoc.com>
2018-03-07 22:20:36 -0500

    Made changes to test package

---
+++ src/test/java/com/edmundsassoc/mcsjSpring/McsjApplicationServerTests.java
@@ -0,0 +1,16 @@
+package com.edmundsassoc.mcsjSpring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class McsjApplicationServerTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
