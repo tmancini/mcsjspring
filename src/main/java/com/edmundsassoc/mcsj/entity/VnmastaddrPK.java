46cb8726ef764cccdf0bcff8da1d19578c43dce7
TomM <TomM@TOMM.edmundsassoc.com>
2018-04-06 20:53:28 -0400

    Reorganized packages

---
+++ src/main/java/com/edmundsassoc/mcsj/entity/VnmastaddrPK.java
@@ -0,0 +1,57 @@
+package com.edmundsassoc.mcsj.entity;
+
+import java.io.Serializable;
+import javax.persistence.Embeddable;
+
+/**
+ * The primary key class for the vnmastaddr database table.
+ * 
+ */
+@Embeddable
+public class VnmastaddrPK implements Serializable {
+  // default serial version id, required for serializable classes.
+  private static final long serialVersionUID = 1L;
+
+  private String vnm_Id;
+
+  private int seq;
+
+  public VnmastaddrPK() {
+  }
+
+  public String getVnm_Id() {
+    return this.vnm_Id;
+  }
+
+  public void setVnm_Id(String vnm_Id) {
+    this.vnm_Id = vnm_Id;
+  }
+
+  public int getSeq() {
+    return this.seq;
+  }
+
+  public void setSeq(int seq) {
+    this.seq = seq;
+  }
+
+  public boolean equals(Object other) {
+    if(this == other) {
+      return true;
+    }
+    if(!(other instanceof VnmastaddrPK)) {
+      return false;
+    }
+    VnmastaddrPK castOther = (VnmastaddrPK)other;
+    return this.vnm_Id.equals(castOther.vnm_Id) && (this.seq == castOther.seq);
+  }
+
+  public int hashCode() {
+    final int prime = 31;
+    int hash = 17;
+    hash = hash * prime + this.vnm_Id.hashCode();
+    hash = hash * prime + this.seq;
+
+    return hash;
+  }
+}
---
+++ src/main/java/com/edmundsassoc/mcsj/entity/Vnmastcommodity.java
@@ -0,0 +1,41 @@
+package com.edmundsassoc.mcsj.entity;
+
+import java.io.Serializable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.NamedQuery;
+
+/**
+ * The persistent class for the vnmastcommodity database table.
+ * 
+ */
+@Entity
+@NamedQuery(name = "Vnmastcommodity.findAll", query = "SELECT v FROM Vnmastcommodity v")
+public class Vnmastcommodity implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  @EmbeddedId
+  private VnmastcommodityPK id;
+
+  private String commodity_Code;
+
+  public Vnmastcommodity() {
+  }
+
+  public VnmastcommodityPK getId() {
+    return this.id;
+  }
+
+  public void setId(VnmastcommodityPK id) {
+    this.id = id;
+  }
+
+  public String getCommodity_Code() {
+    return this.commodity_Code;
+  }
+
+  public void setCommodity_Code(String commodity_Code) {
+    this.commodity_Code = commodity_Code;
+  }
+
+}
